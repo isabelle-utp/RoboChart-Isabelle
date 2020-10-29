6cb2c21aa5795a2714acc907ac8d6aee1e9c3ea2
simondfoster <simondf@gmail.com>
2020-10-29 16:26:22 +0900

    Added type name inner syntax antiquotation

--- Isabelle-API/Inner_Antiquotations.thy
+++ Isabelle-API/Inner_Antiquotations.thy
@@ -6,13 +6,20 @@ begin
 
 syntax
   "_inner_const_name" :: "id \<Rightarrow> logic" ("@{const'_name _}")
+  "_inner_type_name" :: "type \<Rightarrow> logic" ("@{type'_name _}")
 
 parse_translation \<open>
   [(@{syntax_const "_inner_const_name"}, 
     fn ctx => fn terms => 
     case terms of [Free (n, _)] => 
       let val Const (c, _) = Proof_Context.read_const {proper = true, strict = false} ctx n
-      in HOLogic.mk_literal c end)]
+      in HOLogic.mk_literal c end)
+  ,(@{syntax_const "_inner_type_name"}, 
+    fn ctx => fn terms => 
+    case terms of [Const (n, _)] => 
+      let val Type (c, _) = Proof_Context.read_type_name {proper = true, strict = false} ctx (Lexicon.unmark_type n)
+      in HOLogic.mk_literal c end)
+]
 \<close>
 
 end
