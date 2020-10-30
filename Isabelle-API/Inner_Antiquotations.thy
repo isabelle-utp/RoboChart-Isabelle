a9028042ab3ad436881bc1a6cc5891918b4b6a69
simondfoster <simondf@gmail.com>
2020-10-30 11:52:29 +0900

    Added an antiquotation for types

--- Isabelle-API/Inner_Antiquotations.thy
+++ Isabelle-API/Inner_Antiquotations.thy
@@ -6,10 +6,25 @@ begin
 
 syntax
   "_inner_const_name" :: "id \<Rightarrow> logic" ("@{const'_name _}")
-  "_inner_type_name" :: "type \<Rightarrow> logic" ("@{type'_name _}")
+  "_inner_type_name"  :: "type \<Rightarrow> logic" ("@{type'_name _}")
+  "_inner_typ"        :: "type \<Rightarrow> logic" ("@{typ _}")
+
+ML \<open> 
+\<close>
 
 parse_translation \<open>
-  [(@{syntax_const "_inner_const_name"}, 
+let
+  open Syntax;
+  open HOLogic;
+  fun 
+    lift_typ (Type (n, ps)) = 
+      const @{const_name Type} $ mk_literal n $ mk_list dummyT (map lift_typ ps) |
+    lift_typ (TFree (n, sort)) = 
+      const @{const_name TFree} $ mk_literal n $ mk_list dummyT (map mk_literal sort) |
+    lift_typ (TVar ((n, i), sort)) =
+      const @{const_name TVar} $ (mk_prod (mk_literal n, mk_numeral i)) $ mk_list dummyT (map mk_literal sort)
+in
+  [(@{syntax_const "_inner_const_name"},
     fn ctx => fn terms => 
     case terms of [Free (n, _)] => 
       let val Const (c, _) = Proof_Context.read_const {proper = true, strict = false} ctx n
@@ -19,7 +34,12 @@ parse_translation \<open>
     case terms of [Const (n, _)] => 
       let val Type (c, _) = Proof_Context.read_type_name {proper = true, strict = false} ctx (Lexicon.unmark_type n)
       in HOLogic.mk_literal c end)
+  ,(@{syntax_const "_inner_typ"},
+    fn ctx => fn terms => 
+    case terms of [t] => 
+      lift_typ (Syntax_Phases.decode_typ t))
 ]
+end
 \<close>
 
 end
--- Isabelle-API/Syntax.thy
+++ Isabelle-API/Syntax.thy
@@ -6,11 +6,13 @@ begin
 
 subsection \<open> Functions \<close>
 
-consts read_term :: "Proof.context \<Rightarrow> String.literal \<Rightarrow> term"
+consts parse_term :: "Proof.context \<Rightarrow> String.literal \<Rightarrow> term"
+consts read_term  :: "Proof.context \<Rightarrow> String.literal \<Rightarrow> term"
 
 subsection \<open> Code Generation Axioms \<close>
 
 code_printing
+  constant "parse_term" \<rightharpoonup> (SML) "Syntax.parse'_term" |
   constant "read_term" \<rightharpoonup> (SML) "Syntax.read'_term"
 
 end
