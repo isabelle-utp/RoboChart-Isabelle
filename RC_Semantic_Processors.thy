5d3a2e8929a3f65d9d105d32129ef9550e5a282b
simondfoster <simondf@gmail.com>
2020-11-20 12:41:09 +0900

    Started reworking semantic processors

---
+++ RC_Semantic_Processors.thy
@@ -0,0 +1,38 @@
+theory RC_Semantic_Processors
+  imports RoboChart_AST "HOL.Real"
+begin
+
+ML \<open> Syntax.install_operations \<close>
+
+type_synonym PredType = "typ \<Rightarrow> typ"
+type_synonym ActionType = "typ \<Rightarrow> typ \<Rightarrow> typ"
+type_synonym ProbType = "typ \<Rightarrow> typ"
+
+definition "null_predT = (\<lambda> _. @{typ bool})"
+definition "null_actionT = (\<lambda> _ _. @{typ bool})"
+definition "null_probT = (\<lambda> _. @{typ real})"
+
+record RCSem_Proc = 
+  predT      :: PredType
+  pred_syn   :: "String.literal"
+  actionT    :: ActionType
+  action_syn :: "String.literal"
+  probT      :: ProbType
+  prob_syn   :: "String.literal"
+  itf_sem    :: "Interface \<Rightarrow> theory \<Rightarrow> theory"
+  rpl_sem    :: "RoboticPlatform \<Rightarrow> theory \<Rightarrow> theory"
+  stm_sem    :: "StateMachineDef \<Rightarrow> theory \<Rightarrow> theory"
+
+definition "null_RCSem_Proc =
+  \<lparr> predT = null_predT
+  , pred_syn = STR ''any''
+  , actionT = null_actionT
+  , action_syn = STR ''any''
+  , probT = null_probT
+  , prob_syn = STR ''any''
+  , itf_sem = (\<lambda> _. id)
+  , rpl_sem = (\<lambda> _. id)
+  , stm_sem = (\<lambda> _. id)
+  \<rparr>"
+
+end
